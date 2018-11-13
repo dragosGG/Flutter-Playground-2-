65bb36d4d11e0b77a22c61670cd25b49d2ad3a0f
Dragos <dragos.giugula@gmail.com>
2018-11-13 11:31:23 +0100

    input field

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -10,11 +10,36 @@ class LoginScreen extends StatefulWidget {
 
 
 class LoginScreenState extends State <LoginScreen> {
-  Widget build() {
+  Widget build(context) {
     return Container(
       margin: EdgeInsets.all(20.0),
-      child: Text("Login Screen"),
+      child: Form(
+        child: Column(
+          children: [emailField(),
+          passwordField(),
+          submitButton()],
+        ),
+      ),
 
     );
   }
+}
+
+
+Widget emailField() {
+  return TextFormField(
+    decoration: InputDecoration(
+      labelText: 'Email Address',
+      hintText: 'you@example.com',
+    ),
+  );
+
+}
+
+Widget passwordField() {
+
+}
+
+Widget submitButton() {
+
 }
